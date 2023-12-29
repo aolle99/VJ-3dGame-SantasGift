b9398ea45d40b10c80ce6774b25edd7c411470fc
crismigo <cristinamigo01@gmail.com>
2023-12-30 00:14:32 +0100

    UI main menu i level algo fet

---
+++ Assets/UI/UIMainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UI
+{
+    public class UIMainMenu : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+            
+            Button button = root.Q<Button>("Play");
+            //when button clicked load firstlevel sce
+            button.clicked += () =>
+            {
+                print("clicked");
+                UnityEngine.SceneManagement.SceneManager.LoadScene("FirstLevel");
+            };
+            
+            Button quitButton = root.Q<Button>("Quit");
+            //when button clicked quit
+            quitButton.clicked += () =>
+            {
+                print("quit");
+                Application.Quit();
+            };
+        }
+    }
+}
