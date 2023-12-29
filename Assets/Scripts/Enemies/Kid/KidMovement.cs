2268eea703e00281219538f839a9932389cee4dc
crismigo <cristinamigo01@gmail.com>
2023-12-29 19:23:20 +0100

    Kid millorat i UI del level

--- Assets/Scripts/Enemies/Kid/KidMovement.cs
+++ Assets/Scripts/Enemies/Kid/KidMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,17 +9,30 @@ namespace Enemies.Kid
     public class KidMovement : MonoBehaviour
     {
         [FormerlySerializedAs("rotationSpeed")] public float movementSpeed;
-        public float gravityScale, fallingGravityScale, jumpForce;
+        public float gravityScale, jumpForce;
         private float _angle = 0f;
         private Rigidbody _rb;
-        
-        private static readonly int Speed = Animator.StringToHash("speed");
         private Animator _anim;
+        private Boolean _jumping = false;
+        private Boolean _canJump = true;
+        private CharacterController _charControl;
+        private Vector3 _startDirection;
+        private float _speedY;
+        
         private static readonly int Jump = Animator.StringToHash("jump");
+        
+        public bool ViewDirection { get; private set; }
 
         // Start is called before the first frame update
         void Start()
         {
+            _charControl = GetComponent<CharacterController>();
+            var playerTransform = transform;
+            var parent = playerTransform.parent;
+            _startDirection = playerTransform.position - parent.position;
+            ViewDirection = false;
+            _speedY = 0;
+            
             _anim = GetComponentInChildren<Animator>();
             _rb = GetComponent<Rigidbody>();
         }
@@ -26,71 +40,87 @@ namespace Enemies.Kid
         // Update is called once per frame
         void Update()
         {
-            
+            //if(_anim.GetBool(Jump)) print(_anim.GetBool(Jump));
         }
-    
-        void ManageMovement()
-        {
-            Vector3 position = transform.position;
-            Vector3 parentPosition = transform.parent.position;
-            Vector3 direction = position - parentPosition;
-            Quaternion rotation = Quaternion.AngleAxis(_angle, Vector3.up);
-            Vector3 target = parentPosition + rotation * direction;
-            float rotationY = Mathf.Atan2(target.z, target.x) * Mathf.Rad2Deg + 180f;
-            
-            rotation = Quaternion.Euler(0, -rotationY, 0f);
-            _rb.Move(target, rotation);
-            
-            _angle = movementSpeed * Time.deltaTime;
-            _anim.SetFloat(Speed, 1);
-        }
-    
-        private void OnCollisionEnter(Collision collision)
-        {
-            
-            if (collision.gameObject.CompareTag("House"))
-            {
-                //print("House Collision");
-            }
-            
-        }
-    
+        
         private void OnTriggerEnter(Collider other)
         {
+            Vector3 sideDirection = Vector3.forward;
+            Vector3 directionToOther = other.transform.position - transform.position;
+            directionToOther.Normalize();
+            print("collider");
+            print(other.gameObject.CompareTag("House"));
             if (other.gameObject.CompareTag("House"))
             {
-                //print("House Trigger");
-                ManageJump();
+                print("house");
+                _jumping = true;
             }
+        }
+        
+        void FixedUpdate()
+        {
+            ManageMovement();
+            ManageOrientation();
+            ManageJump(); 
+        }
+        
+        void ManageMovement()
+        {
+            var position = transform.position;
+            _angle = movementSpeed * Time.deltaTime;
+            var direction = position - transform.parent.position;
+            direction = Quaternion.AngleAxis(_angle, Vector3.up) * direction;
+            if (_charControl.Move(direction - position) == CollisionFlags.Sides)
+            {
+                transform.position = position;
+                Physics.SyncTransforms();
+            }
+        }
+        
+        private void ManageOrientation()
+        {
+            var playerTransform = transform;
+            var currentDirection = playerTransform.position - playerTransform.parent.position;
+            currentDirection.y = 0.0f;
+            currentDirection.Normalize();
+             
+            Quaternion orientation;
+            if ((_startDirection - currentDirection).magnitude < 1e-3)
+                orientation = Quaternion.AngleAxis(0.0f, Vector3.up);
+            else if ((_startDirection + currentDirection).magnitude < 1e-3)
+                orientation = Quaternion.AngleAxis(180.0f, Vector3.up);
+            else
+                orientation = Quaternion.FromToRotation(_startDirection, currentDirection);
+
+            orientation.eulerAngles = new Vector3(0.0f, orientation.eulerAngles.y, 0.0f);
+            transform.rotation = orientation;
+
+            if (!ViewDirection) transform.Rotate(Vector3.up, 180.0f);
         }
         
         void ManageJump()
         {
-            //print("jumping");
-            _anim.SetBool(Jump, true);
-            _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            //_anim.SetBool(Jump, false);
-        }
-    
-        void FixedUpdate()
-        {
-            ManageMovement();
-            ApplyGravity();
-        }
-    
-        void ApplyGravity()
-        {
-            if (_rb.velocity.y < 0)
+            var position = transform.position;
+            if (_charControl.Move(_speedY * Time.deltaTime * Vector3.up) != CollisionFlags.None)
             {
-                _rb.velocity += Vector3.up * (Physics.gravity.y * (fallingGravityScale - 1f) * Time.deltaTime);
-                
+                transform.position = position;
+                Physics.SyncTransforms();
             }
-            else if (_rb.velocity.y > 0)
+            
+            if (_charControl.isGrounded)
             {
-                _rb.velocity += Vector3.up * (Physics.gravity.y * (gravityScale - 1f) * Time.deltaTime); 
+                _anim.SetBool(Jump, false);
+                _canJump = true;
+                if (_speedY < 0.0f) _speedY = 0.0f;
+                if(_canJump && _jumping)
+                {
+                    _speedY = jumpForce;
+                    _canJump = false;
+                    _jumping = false;
+                    _anim.SetBool(Jump, true);
+                }
             }
+            else _speedY -= gravityScale * Time.deltaTime;
         }
-    
     }
-
 }
