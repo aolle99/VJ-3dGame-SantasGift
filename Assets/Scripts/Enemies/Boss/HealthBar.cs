55cc7b3dc2c8aa00b1c1edb2032802a1c8d3bfab
crismigo <cristinamigo01@gmail.com>
2024-01-02 21:30:48 +0100

    init healthbar boss

---
+++ Assets/Scripts/Enemies/Boss/HealthBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Enemies.Boss
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] private Image _healthbarSprite;
+        
+        public void UpdeateHealthBar(float maxHealth, float currentHealth)
+        {
+            _healthbarSprite.fillAmount = currentHealth / maxHealth;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,15 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Enemies.Boss;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
+        private float _currentHealth;
+        private float _maxHealth;
+        [SerializeField]private HealthBar healthBar;
         // Start is called before the first frame update
         void Start()
         {
-            
+            _maxHealth = 100f;
+            _currentHealth = _maxHealth;
+            healthBar.UpdeateHealthBar(_maxHealth, _currentHealth);
+        }
+
+        private void OnMouseDown()
+        {
+            _currentHealth -= 10f;
+            healthBar.UpdeateHealthBar(_maxHealth, _currentHealth);
         }
 
         // Update is called once per frame
