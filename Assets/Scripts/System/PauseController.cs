08c2b57d89190996288a021ea68fb76ec34802ef
aolle99 <aolle99@gmail.com>
2024-01-03 11:41:03 +0100

    Reordenados los ficheros Manager

---
+++ Assets/Scripts/System/PauseController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace System
+{
+    public class PauseController : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                GameState currentGameState = GameStateManager.Instance.CurrentGameState;
+                GameState newGameState = currentGameState == GameState.Gameplay ? GameState.Paused : GameState.Gameplay;
+            
+                GameStateManager.Instance.SetGameState(newGameState);
+            }
+        }
+    }
+}
