6e7890d9c186d676c23dc116d7047a6f08bb120b
aolle99 <aolle99@gmail.com>
2024-01-03 11:37:57 +0100

    S'ha fet un script per a moure la camara 2 de manera lliure amb les fletxes del teclat, així es mes facil veure l'execució del programa

---
+++ Assets/Scripts/Camera/FreeCamMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Camera
+{
+    public class FreeCamMove : MonoBehaviour
+    {
+        public Transform target;
+        public float rotationSpeed = 5f;
+        public float translationSpeed = 2f;
+        
+        void Update()
+        {
+            // Rotación alrededor del cilindro
+            float horizontalInput = Input.GetAxis("Horizontal")*-1;
+            transform.RotateAround(target.position, Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+
+
+            // Movimiento en el eje Y
+            float verticalInput = Input.GetAxis("Vertical");
+            transform.Translate(Vector3.up * (verticalInput * translationSpeed * Time.deltaTime));
+        }
+    }
+}
