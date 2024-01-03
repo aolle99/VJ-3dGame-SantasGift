903dfaf72bd5f4eac794cc37f4279b847150b346
aolle99 <aolle99@gmail.com>
2024-01-03 17:37:23 +0100

    Interacción con el puente y tutorial

---
+++ Assets/Scripts/Environment/InteractionSystem/Bridge.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Environment.InteractionSystem
+{
+    public class Bridge : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private string prompt;
+        
+        public string InteractionPrompt => prompt;
+        
+        [SerializeField] private InteractionPromptUI _interactionPromptUI;
+        
+        public InteractionPromptUI InteractionPromptUI
+        {
+            get => _interactionPromptUI;
+            set => _interactionPromptUI = value;
+        }
+
+        public bool Interact(Interactor interactor)
+        {
+            Debug.Log("Interacted with bridge");
+            return true;
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/InteractionSystem/Tutorial.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Environment.InteractionSystem
+{
+    public class Tutorial : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private string prompt;
+        
+        public string InteractionPrompt => prompt;
+        
+        [SerializeField] private InteractionPromptUI _interactionPromptUI;
+        
+        public InteractionPromptUI InteractionPromptUI
+        {
+            get => _interactionPromptUI;
+            set => _interactionPromptUI = value;
+        }
+
+        public bool Interact(Interactor interactor)
+        {
+            return true;
+        }
+    }
+}
