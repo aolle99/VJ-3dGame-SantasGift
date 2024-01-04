a5a02758ca05adb4964c8f82fdec3f4eb34a38d9
aolle99 <aolle99@gmail.com>
2024-01-04 20:14:41 +0100

    S'han arreglat els regals que es llencen durant el que han de durar

--- Assets/Scripts/Player/GiftBullet/GiftController.cs
+++ Assets/Scripts/Player/GiftBullet/GiftController.cs
@@ -3,14 +3,30 @@ using UnityEngine;
 
 namespace Player.GiftBullet
 {
+    enum LifeTimeType
+    {
+        Time,
+        Distance
+    }
     public class GiftController : MonoBehaviour
     {
         // Start is called before the first frame update
-        public int direction = 1;
-        public float speed = 5.0f;
-        float angle = 0f;
-        public float bulletDuration = 1f;
-        float lifeTime = 0f;
+        
+        [SerializeField]private float speed = 5.0f;
+
+        [SerializeField] private LifeTimeType lifeTimeType = LifeTimeType.Time;
+        [SerializeField] private float bulletDuration = 1f;
+        [SerializeField] private int bulletAngleDistance = 180;
+        [SerializeField] private float damage = 1f;
+        
+        private float lifeTime = 0f;
+        private float _startAngle;
+        private float direction = 1f;
+        private float angle = 0f;
+        private float acumulatedAngle = 0f;
+        private Vector2 startPosition;
+        private Vector2 centerPosition;
+        private float startAngle;
         
         Rigidbody rb;
         void Start()
@@ -18,6 +34,30 @@ namespace Player.GiftBullet
             var position = transform.position;
             angle = Mathf.Atan2(position.z, position.x);
             rb = GetComponent<Rigidbody>();
+            _startAngle = angle * Mathf.Rad2Deg;
+            
+            startPosition = new Vector2(position.x, position.z);
+            
+            startAngle = calculateAngle();
+            
+            
+        }
+
+        private void Update()
+        {
+            if (lifeTimeType == LifeTimeType.Distance)
+            {
+                float newAngle = calculateAngle();
+                acumulatedAngle += Mathf.Abs(newAngle - startAngle) * Mathf.Rad2Deg;
+                startAngle = newAngle;
+                
+                // Verificar si el ángulo supera el umbral
+                if (acumulatedAngle  > bulletAngleDistance)
+                {
+                    Destroy(gameObject);
+                }
+            }
+
         }
 
         // Update is called once per frame
@@ -33,9 +73,25 @@ namespace Player.GiftBullet
 
             rb.position = new Vector3(x, transform.position.y, z) ;
 
-            lifeTime += Time.deltaTime;
+            if (lifeTimeType == LifeTimeType.Time)
+            {
+                lifeTime += Time.deltaTime;
 
-            if (lifeTime > bulletDuration)
+                if (lifeTime > bulletDuration)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Enemy"))
+            {
+                // Call the enemy hit method
+                Destroy(gameObject);
+            }
+            if (collision.gameObject.CompareTag("Obstacle"))
             {
                 Destroy(gameObject);
             }
@@ -45,13 +101,29 @@ namespace Player.GiftBullet
         {
             if (other.gameObject.CompareTag("Enemy"))
             {
-                Destroy(other.gameObject);
+                // Call the enemy hit method
                 Destroy(gameObject);
             }
             if (other.gameObject.CompareTag("Obstacle"))
             {
                 Destroy(gameObject);
             }
+        }
+        
+        public void SetDirection(float d)
+        {
+            direction = d;
+        }
+
+        private float calculateAngle()
+        {
+            // Calcular el ángulo acumulado
+            var currentPosition = new Vector2(transform.position.x, transform.position.z);
+            float ladoC = Vector2.Distance(startPosition, currentPosition);
+            float ladoA = Vector2.Distance(startPosition, centerPosition);
+            float ladoB = Vector2.Distance(currentPosition, centerPosition);
+                
+            return Mathf.Acos((Mathf.Pow(ladoA, 2) + Mathf.Pow(ladoB, 2) - Mathf.Pow(ladoC, 2)) / (2 * ladoA * ladoB));
         }
     }
 }
--- Assets/Scripts/Player/PlayerThrowGift.cs
+++ Assets/Scripts/Player/PlayerThrowGift.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Player.GiftBullet;
 using UnityEngine;
 
@@ -6,38 +7,86 @@ namespace Player
 {
     public class PlayerThrowGift : MonoBehaviour
     {
-        [SerializeField] private GameObject bulletPrefab;
+        [SerializeField] private GameObject blueBulletPrefab;
+        [SerializeField] private GameObject redBulletPrefab;
         [SerializeField] private float relativeVerticalPosition = 2f;
+        [SerializeField] private float timeBetweenThrowsBlue = 0.6f;
+        [SerializeField] private float timeBetweenThrowsRed = 1.2f;
         private GiftStateManager _giftStateManager;
         private PlayerMovement _movePlayer;
+        private bool _canThrow = true;
+        private float _coldDownTimer;
+        
+        private Animator anim;
+        
+        private static readonly int AnimThrow = Animator.StringToHash("isThrowing");
         
         private void Start()
         {
             _movePlayer = GetComponent<PlayerMovement>();
             _giftStateManager = GiftStateManager.Instance;
+            anim = GetComponentInChildren<Animator>();
         }
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Mouse0))
+            if (!_canThrow)
             {
+                _coldDownTimer += Time.deltaTime;
+                var timeBetweenThrows = _giftStateManager.GetAmmunitionSelected() == GiftType.Blue ? timeBetweenThrowsBlue : timeBetweenThrowsRed;
+                if (_coldDownTimer >= timeBetweenThrows)
+                {
+                    _canThrow = true;
+                    _coldDownTimer = 0;
+                }
+            }
+            
+            if (Input.GetKeyDown(KeyCode.Mouse0) && _canThrow)
+            {
+                anim.SetBool(AnimThrow, true);
                 GiftType ammunitionSelected = _giftStateManager.GetAmmunitionSelected();
                 // If there is no ammunition, the player cannot shoot
-                if(ammunitionSelected == GiftType.Blue && _giftStateManager.GetBlueGifts() <= 0) return;
-                if(ammunitionSelected == GiftType.Red && _giftStateManager.GetRedGifts() <= 0) return;
+                var prefab = ammunitionSelected == GiftType.Blue ? blueBulletPrefab : redBulletPrefab;
+                if (ammunitionSelected == GiftType.Blue && _giftStateManager.GetBlueGifts() <= 0)
+                {
+                    // Reproduce the sound of no ammunition
+                    return;
+                }
+
+                if (ammunitionSelected == GiftType.Red && _giftStateManager.GetRedGifts() <= 0)
+                {
+                    // Reproduce the sound of no ammunition
+                    return;
+                }
                 
-                // Create the bullet
-                var position = transform.position + new Vector3(0, relativeVerticalPosition, 0);
-                GameObject bullet = Instantiate(bulletPrefab, position, Quaternion.identity);
-                var giftController = bullet.GetComponent<GiftController>();
-                if (_movePlayer.ViewDirection) giftController.direction = 1;
-                else giftController.direction = -1;
-                bullet.transform.parent = transform.parent;
+                _canThrow = false;
                 
-                // Update the number of ammunition
-                if (ammunitionSelected == GiftType.Blue) _giftStateManager.RemoveBlueGift();
-                else _giftStateManager.RemoveRedGift();
+                // If there is ammunition, the player can shoot
+                StartCoroutine(ThrowGiftCoroutine(prefab, ammunitionSelected));
+                
             }
+        }
+        IEnumerator ThrowGiftCoroutine(GameObject prefab, GiftType ammunitionSelected)
+        {
+            yield return new WaitForSeconds(0.3f);
+            
+            // Create the bullet
+            var position = transform.position + new Vector3(0, relativeVerticalPosition, 0);
+            GameObject bullet = Instantiate(prefab, position, Quaternion.identity);
+            var giftController = bullet.GetComponent<GiftController>();
+            if (!_movePlayer.ViewDirection) giftController.SetDirection(-1);
+            bullet.transform.parent = transform.parent;
+                
+            // Update the number of ammunition
+            if (ammunitionSelected == GiftType.Blue) _giftStateManager.RemoveBlueGift();
+            else _giftStateManager.RemoveRedGift();
+            
+            Invoke(nameof(StopAnimation), 0.25f);
+        }
+        
+        private void StopAnimation()
+        {
+            anim.SetBool(AnimThrow, false);
         }
     }
 }
