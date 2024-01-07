92428024bc47113ac8f672a3b06040c3e8ab69f6
aolle99 <aolle99@gmail.com>
2024-01-07 12:23:14 +0100

    Ja funciona el trineu per canviar d'escena i petits ajustos

--- Assets/Scripts/Environment/InteractionSystem/Sled.cs
+++ Assets/Scripts/Environment/InteractionSystem/Sled.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using Camera;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Environment.InteractionSystem
 {
     public class Sled : MonoBehaviour, IInteractable
     {
         [SerializeField] private string prompt;
+        [SerializeField] private Scene nextScene;
+        
+        [SerializeField ] private CameraTransition cameraTransition;
         
         public string InteractionPrompt => prompt;
         
@@ -18,7 +23,18 @@ namespace Environment.InteractionSystem
 
         public bool Interact(Interactor interactor)
         {
+            if (cameraTransition)
+            {
+                cameraTransition.StartFadeIn();
+            }
+            Invoke(nameof(LoadNextScene), 1.5f);
+            
             return true;
+        }
+        
+        public void LoadNextScene()
+        {
+            SceneManager.LoadScene(nextScene.name);
         }
     }
 }
