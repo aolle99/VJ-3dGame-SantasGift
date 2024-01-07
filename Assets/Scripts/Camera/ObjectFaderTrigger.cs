d36f6c05eb5908930ff5a3279b30dc817f0bdcd0
aolle99 <aolle99@gmail.com>
2024-01-07 17:55:22 +0100

    Mejoras

--- Assets/Scripts/Camera/ObjectFaderTrigger.cs
+++ Assets/Scripts/Camera/ObjectFaderTrigger.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Environment;
 using UnityEditor;
@@ -15,23 +16,23 @@ namespace Camera
 
         private void Update()
         {
-            RaycastHit[] hits;
-            
-            Vector3 playerPosition = playerTransform.position;
-            Vector3 camPosition = transform.position;
-            Vector3 direction = playerPosition - camPosition;
-            
-            hits = Physics.RaycastAll(camPosition, (playerPosition - camPosition).normalized, detectionDistance);
-
-            foreach (RaycastHit hit in hits)
+            if (MapManager.instance.GetCurrentFaseRadius() < 70)
             {
-                ObjectFader objectFader = hit.collider.GetComponent<ObjectFader>();
-                if (objectFader == null) continue;
-                objectFader.SetFade();
+                RaycastHit[] hits;
+            
+                Vector3 playerPosition = playerTransform.position;
+                Vector3 camPosition = transform.position;
+                Vector3 direction = (playerPosition - camPosition).normalized;
+            
+                hits = Physics.RaycastAll(camPosition, direction, detectionDistance);
+
+                foreach (RaycastHit hit in hits)
+                {
+                    ObjectFader objectFader = hit.collider.GetComponent<ObjectFader>();
+                    if (objectFader == null) continue;
+                    objectFader.SetFade();
+                }
             }
-            
-            
-            
         }
     }
 }
